4c8291f58b96851158ccdac9f79a0b8b94bff722
DengZhiJie <363810386@qq.com>
2015-09-24 14:51:24 +0800

    //点击Index进去的界面

--- app/src/main/java/com/projectforandroid/ui/fragment/TopLineFragment.java
+++ app/src/main/java/com/projectforandroid/ui/fragment/TopLineFragment.java
@@ -1,31 +1,35 @@
 package com.projectforandroid.ui.fragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import com.projectforandroid.R;
 import com.projectforandroid.adapter.FragmentAdapter;
+import com.projectforandroid.ui.activity.TopLine;
 import java.util.List;
 
 /**
  * Created by 杰 on 2015/9/21.
  */
 public class TopLineFragment extends Fragment{
-   /* //定义静态方法ViewHolder
+    //定义静态方法ViewHolder
     static class ViewHolder{
         public ImageView Header;
         public TextView Title;
         public TextView Content;
         public ImageView img_jiantou;
 
-    }*/
+    }
     public String[] itemtitle = new String[] {
         "This is item1 title", "This is item2 title", "This is item3 title", "This is item4 title",
         "This is item5 title", "This is item6 title", "This is item7 title"
@@ -43,7 +47,7 @@ public class TopLineFragment extends Fragment{
         Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_topline, null);
         ListView listView=(ListView)view.findViewById(R.id.topline_listview);
-       /* //定义一个Baseadapter
+       ///定义一个Baseadapter
         BaseAdapter adapter=new BaseAdapter() {
 
             @Override
@@ -81,10 +85,16 @@ public class TopLineFragment extends Fragment{
                 holder.img_jiantou.setImageResource(R.drawable.btn_jiantou);
                 return convertView;
             }
-        };*/
-        // TODO: 2015/9/24  
-        FragmentAdapter adapter=new FragmentAdapter(this,);
+        };
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent=new Intent();
+                intent.setClass(getActivity(),TopLine.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
